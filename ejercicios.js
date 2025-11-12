839ee5753038b073b5c61befbadead48e1bcf7d4
hum55 <humberto.ramirez@tecdesoftware.edu.mx>
2025-11-11 22:03:58 -0600

    ejercicio 5.1 resuelto

--- ejercicios.js
+++ ejercicios.js
@@ -303,15 +303,15 @@ const invertido = [];
  */
 function crearMatriz(filas, columnas) {
   // TODO: Crea una matriz de filas x columnas llena de ceros
-  const matriz = [];
-    for (let i = 0; i < filas; i++) {
-        const fila = [];
-        for (let j = 0; j < columnas; j++) {
-            fila.push(0);
-        }
-        matriz.push(fila);
-    }
-    return matriz;
+  const matriz = []; 
+    for (let i = 0; i < filas; i++) { 
+        const fila = []; 
+        for (let j = 0; j < columnas; j++) { 
+            fila.push(0); 
+        } 
+        matriz.push(fila); 
+    } 
+    return matriz; 
 
 }
 

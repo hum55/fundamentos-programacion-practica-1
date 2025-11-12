075adbd7bfbfb6d28b714cf7dcae5df7e91df051
hum55 <humberto.ramirez@tecdesoftware.edu.mx>
2025-11-11 21:53:04 -0600

    ejercicio 2.3 resuelto

--- ejercicios.js
+++ ejercicios.js
@@ -106,13 +106,13 @@ function evaluarNota(nota) {
  * @returns {number} - El nÃºmero mayor
  */
 function mayorDeTres(a, b, c) {
-  if (a>=b & a>=c) {
-    return a
-  } else if (b>=a & b>=c){
-    return b
-  } else {
-    return c
-  }
+  if (a>=b & a>=c) { 
+    return a 
+  } else if (b>=a & b>=c){ 
+    return b 
+  } else { 
+    return c 
+  } 
   
 }
 
